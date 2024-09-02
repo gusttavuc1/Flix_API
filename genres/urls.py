a0f429b8f50020305a1526bd2f74ab128af7dd1b
gusttavuc1 <gusttavuc1@hotmail.com>
2024-09-02 20:07:33 -0300

    Commit até o módulo 19

--- genres/urls.py
+++ genres/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views
-from .views import genre_view
+from .views import genre_create_list_view, genre_detail_view
+
+
+app_name = 'genres'  # Opcional, mas útil para namespaces
 
 urlpatterns = [
-    path('genres/', genre_view, name='genre_list'),
+    path('', genre_create_list_view, name='index'),  # Página inicial para 'genres'
+    path('<int:pk>/', genre_detail_view, name='genre_detail_view'),  # Detalhes de um gênero específico
     # Adicione outras URLs do aplicativo aqui
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,42 @@
 from django.http import JsonResponse
 from .models import Genre
-# Create your views here.
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 
-def genre_view(request):
-    genres = Genre.objects.all()
-    return JsonResponse(genres)
+
+# Create your views here.
+@csrf_exempt
+def genre_create_list_view(request):
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name = data['name'])
+        new_genre.save()
+        return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201,)
+
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    try:
+        genre = get_object_or_404(Genre, pk=pk)
+    except Exception as e:
+        print(f"Error finding genre: {e}")
+        return JsonResponse({'error': 'Genre not found'}, status=404)
     
+    if request.method == 'GET':
+        data = {'id': genre.id, 'name': genre.name}
+        return JsonResponse(data)
+        
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genre.name = data.get('name')
+        genre.save()
+        return JsonResponse({'id': genre.id, 'name': genre.name})
+    
+    elif request.method == 'DELETE':
+        genre.delete()
+        return JsonResponse({'message': 'Genero Excluido com sucesso.'}, status=204,)
